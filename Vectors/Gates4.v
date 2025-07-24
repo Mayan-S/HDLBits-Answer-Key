83c9eb633f095a73d2b2c44edd3394f871d52836
Mayan Saravanabavan <mayansaravanabavan@gmail.com>
2025-07-24 15:15:38 -0400

    Basic Gates

--- Vectors/Gates4.v
+++ Vectors/Gates4.v
@@ -4,9 +4,8 @@ module top_module(
     output out_or,
     output out_xor
 );
-    
+
     assign out_and = in[3] & in[2] & in[1] & in[0];
     assign out_or = in[3] | in[2] | in[1] | in[0];
     assign out_xor = in[3] ^ in[2] ^ in[1] ^ in[0];
-
 endmodule
